c4ec0d09d4c1a8efbe45b24a77d4d5317470e3fb
Leo0756 <jlortega142@ieszaidinvergeles.org>
2024-02-05 17:38:17 +0100

    Proyecto base

---
+++ src/main/java/org/ieszaidinvergeles/dam/bibliotecaspringboot/BibliotecaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.ieszaidinvergeles.dam.bibliotecaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotecaSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ieszaidinvergeles/dam/bibliotecaspringboot/BibliotecaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ieszaidinvergeles.dam.bibliotecaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
