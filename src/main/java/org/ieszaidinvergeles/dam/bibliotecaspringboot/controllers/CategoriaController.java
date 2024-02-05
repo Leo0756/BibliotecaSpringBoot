579fb1211c0dfaa50e26f448e86d9f1b0525f0ff
Leo0756 <jlortega142@ieszaidinvergeles.org>
2024-02-05 18:26:24 +0100

    Crear los controladores de cada entidad

---
+++ src/main/java/org/ieszaidinvergeles/dam/bibliotecaspringboot/controllers/CategoriaController.java
@@ -0,0 +1,9 @@
+package org.ieszaidinvergeles.dam.bibliotecaspringboot.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/biblioteca/categorias")
+public class CategoriaController {
+}
---
+++ src/main/java/org/ieszaidinvergeles/dam/bibliotecaspringboot/controllers/LibroController.java
@@ -0,0 +1,9 @@
+package org.ieszaidinvergeles.dam.bibliotecaspringboot.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/biblioteca/libros")
+public class LibroController {
+}
