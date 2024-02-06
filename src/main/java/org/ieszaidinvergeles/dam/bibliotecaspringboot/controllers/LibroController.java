6410571dc0a968a0ea45a74a6f4a5e4b61f3da25
Raul Calero Garcia <raulcalerogarcia2003@gmail.com>
2024-02-06 13:24:22 +0100

    Subida de Libro CRUD

--- src/main/java/org/ieszaidinvergeles/dam/bibliotecaspringboot/controllers/LibroController.java
+++ src/main/java/org/ieszaidinvergeles/dam/bibliotecaspringboot/controllers/LibroController.java
@@ -1,9 +1,80 @@
 package org.ieszaidinvergeles.dam.bibliotecaspringboot.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.ieszaidinvergeles.dam.bibliotecaspringboot.models.entities.EntityLibro;
+import org.ieszaidinvergeles.dam.bibliotecaspringboot.models.repositories.IRepositoryLibro;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/biblioteca/libros")
 public class LibroController {
+    @Autowired //inyección de dependencia: por tanto, será inicializado
+    IRepositoryLibro librosRepository; //creando Spring automáticamente su instancia
+
+    @GetMapping //endpoint para buscar todos
+    // Tipo de solicitud HTTP --> GET
+    public List<EntityLibro> buscarLibros() {
+        // ... (código para buscar todos los libros)
+        return (List<EntityLibro>) librosRepository.findAll();
+    }
+
+    @GetMapping("/{id}") //endpoint para buscar un libro por id
+    public ResponseEntity<EntityLibro> buscarLibroPorId(@PathVariable(value = "id") int id) {
+        // ... (código para buscar un libro por su identificador)
+        Optional<EntityLibro> libro = librosRepository.findById(id);
+        if (libro.isPresent())
+            return ResponseEntity.ok().body(libro.get()); // HTTP 200 OK
+        else return ResponseEntity.notFound().build(); // HTTP 404
+    }
+
+    /*@GetMapping("/{nom_libro}") //endpoint para buscar un libro por nombre
+    public ResponseEntity<EntityLibro> buscarLibroPorNombre(@PathVariable(value = "nom_libro") String nombre) {
+        // ... (código para buscar un departamento por su nombre)
+        Optional<EntityLibro> libro = Optional.ofNullable(librosRepository.findByNomLibro(nombre));
+        if (libro.isPresent())
+            return ResponseEntity.ok().body(libro.get()); // HTTP 200 OK
+        else return ResponseEntity.notFound().build(); // HTTP 404
+    }
+*/
+    @PostMapping //endpoint para añadir un nuevo libro
+    public EntityLibro guardarLibro(@Validated @RequestBody EntityLibro libro) {
+        // ... (código para guardar un nuevo libro)
+        return librosRepository.save(libro); // da error si ya existe
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> borrarLibro (@PathVariable(value = "id") int id) {
+        // ... (código para borrar un libro por su id)
+        Optional<EntityLibro> libro = librosRepository.findById(id);
+        if(libro.isPresent()) {
+            librosRepository.deleteById(id);
+            return ResponseEntity.ok().body("Borrado");
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Tipo de solicitud HTTP --> PUT
+    @PutMapping("/{id}")
+    public ResponseEntity<?> actualizarLibro(@RequestBody EntityLibro nuevoLibro,
+                                                    @PathVariable(value = "id") int id) {
+        Optional<EntityLibro> libro = librosRepository.findById(id);
+        if (libro.isPresent()) {
+            libro.get().setNombre(nuevoLibro.getNombre());
+            libro.get().setAutor(nuevoLibro.getAutor());
+            libro.get().setEditorial(nuevoLibro.getEditorial());
+            libro.get().setCategoria(nuevoLibro.getCategoria());
+            librosRepository.save(libro.get());
+            return ResponseEntity.ok().body("Actualizado");
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- src/main/java/org/ieszaidinvergeles/dam/bibliotecaspringboot/models/repositories/IRepositoryLibro.java
+++ src/main/java/org/ieszaidinvergeles/dam/bibliotecaspringboot/models/repositories/IRepositoryLibro.java
@@ -2,6 +2,11 @@ package org.ieszaidinvergeles.dam.bibliotecaspringboot.models.repositories;
 
 import org.ieszaidinvergeles.dam.bibliotecaspringboot.models.entities.EntityLibro;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IRepositoryLibro extends CrudRepository<EntityLibro, Integer> {
+
+
+
 }
