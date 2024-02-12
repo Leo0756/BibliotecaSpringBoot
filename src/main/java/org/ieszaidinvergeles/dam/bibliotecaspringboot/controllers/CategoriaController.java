cb6e0bb8a7c7e4637858113497dd8745148aedf3
Pablo <pabloatenciano@gmail.com>
2024-02-12 13:33:36 +0100

    SOLUCION ERRORES

--- src/main/java/org/ieszaidinvergeles/dam/bibliotecaspringboot/controllers/CategoriaController.java
+++ src/main/java/org/ieszaidinvergeles/dam/bibliotecaspringboot/controllers/CategoriaController.java
@@ -15,6 +15,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/biblioteca/categorias")
+@CrossOrigin(origins = "http://localhost:3000")
 
 public class CategoriaController {
 
--- src/main/java/org/ieszaidinvergeles/dam/bibliotecaspringboot/controllers/LibroController.java
+++ src/main/java/org/ieszaidinvergeles/dam/bibliotecaspringboot/controllers/LibroController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/biblioteca/libros")
 public class LibroController {
     @Autowired //inyección de dependencia: por tanto, será inicializado
