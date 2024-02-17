bc059f4416baa80b5279a60cd894dcdc919713ac
Leo0756 <jlortega142@ieszaidinvergeles.org>
2024-02-17 16:37:53 +0100

    Añadir los registros de las sentencias para UsuarioController

--- src/main/java/org/ieszaidinvergeles/dam/bibliotecaspringboot/controllers/UsuarioController.java
+++ src/main/java/org/ieszaidinvergeles/dam/bibliotecaspringboot/controllers/UsuarioController.java
@@ -1,7 +1,9 @@
 package org.ieszaidinvergeles.dam.bibliotecaspringboot.controllers;
 
 import jakarta.validation.Valid;
+import org.ieszaidinvergeles.dam.bibliotecaspringboot.controllers.helper.HistoricoHelper;
 import org.ieszaidinvergeles.dam.bibliotecaspringboot.models.entities.EntityUsuario;
+import org.ieszaidinvergeles.dam.bibliotecaspringboot.models.repositories.IRepositoryHistorico;
 import org.ieszaidinvergeles.dam.bibliotecaspringboot.models.repositories.IRepositoryUsuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+
 /**
  * Controlador REST que gestiona las operaciones relacionadas con los usuarios.
  */
@@ -20,6 +23,9 @@ public class UsuarioController {
     @Autowired
     private IRepositoryUsuario usuarioRepository;
 
+    @Autowired
+    private IRepositoryHistorico historicoRepository;
+
     /**
      * Selecciona y devuelve todos los usuarios almacenados en la tabla Usuario.
      *
@@ -27,6 +33,7 @@ public class UsuarioController {
      */
     @GetMapping
     public List<EntityUsuario> buscarUsuarios() {
+        HistoricoHelper.guardarSentencia("ip", historicoRepository, "SELECT * FROM usuario;");
         return (List<EntityUsuario>) usuarioRepository.findAll();
     }
 
@@ -39,6 +46,9 @@ public class UsuarioController {
     @GetMapping("/select/{id}")
     public ResponseEntity<EntityUsuario> buscarUsuarioPorId(@PathVariable(value = "id") int id) {
         Optional<EntityUsuario> usuario = usuarioRepository.findById(id);
+
+        HistoricoHelper.guardarSentencia("IP", historicoRepository, "SELECT * FROM usuario WHERE id = " + id);
+
         if (usuario.isPresent()) {
             return ResponseEntity.ok().body(usuario.get());
         } else {
@@ -56,6 +66,7 @@ public class UsuarioController {
     @PostMapping
     public EntityUsuario insertarUsuario(@Valid @RequestBody EntityUsuario usuario) {
         usuario.setId(0);
+        HistoricoHelper.guardarSentencia("IP", historicoRepository, "INSERT INTO usuario (nombre, apellidos) " + "VALUES (" + usuario.getNombre() + "," + usuario.getApellidos() + ")");
         return usuarioRepository.save(usuario);
     }
 
@@ -69,6 +80,7 @@ public class UsuarioController {
     public ResponseEntity<?> borrarUsuario(@PathVariable(value = "id") int id) {
         Optional<EntityUsuario> usuario = usuarioRepository.findById(id);
         if (usuario.isPresent()) {
+            HistoricoHelper.guardarSentencia("IP", historicoRepository, "DELETE FROM usuario WHERE id=" + id);
             usuarioRepository.deleteById(id);
             return ResponseEntity.ok().body("Borrado");
         } else {
@@ -87,6 +99,7 @@ public class UsuarioController {
     public ResponseEntity<?> actualizarUsuario(@Valid @RequestBody EntityUsuario nuevoUsuario, @PathVariable(value = "id") int id) {
         Optional<EntityUsuario> usuario = usuarioRepository.findById(id);
         if (usuario.isPresent()) {
+            HistoricoHelper.guardarSentencia("IP", historicoRepository, "UPDATE usuario SET " + "nombre =" + nuevoUsuario.getNombre() + "," + "apellidos =" + nuevoUsuario.getApellidos() + "WHERE id=" + id);
             usuario.get().setNombre(nuevoUsuario.getNombre());
             usuario.get().setApellidos(nuevoUsuario.getApellidos());
             usuario.get().setListaPrestamos(nuevoUsuario.getListaPrestamos());
