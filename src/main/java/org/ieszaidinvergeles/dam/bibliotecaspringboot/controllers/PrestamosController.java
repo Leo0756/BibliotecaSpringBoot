83d4f7c84a4a74a7214aae64f4fd37366b9c842f
Leo0756 <jlortega142@ieszaidinvergeles.org>
2024-02-17 17:23:23 +0100

    Añadir sentencias de registro para PrestamosController

--- src/main/java/org/ieszaidinvergeles/dam/bibliotecaspringboot/controllers/PrestamosController.java
+++ src/main/java/org/ieszaidinvergeles/dam/bibliotecaspringboot/controllers/PrestamosController.java
@@ -1,9 +1,11 @@
 package org.ieszaidinvergeles.dam.bibliotecaspringboot.controllers;
 
 import jakarta.validation.Valid;
+import org.ieszaidinvergeles.dam.bibliotecaspringboot.controllers.helper.HistoricoHelper;
 import org.ieszaidinvergeles.dam.bibliotecaspringboot.models.entities.EntityCategoria;
 import org.ieszaidinvergeles.dam.bibliotecaspringboot.models.entities.EntityLibro;
 import org.ieszaidinvergeles.dam.bibliotecaspringboot.models.entities.EntityPrestamos;
+import org.ieszaidinvergeles.dam.bibliotecaspringboot.models.repositories.IRepositoryHistorico;
 import org.ieszaidinvergeles.dam.bibliotecaspringboot.models.repositories.IRepositoryLibro;
 import org.ieszaidinvergeles.dam.bibliotecaspringboot.models.repositories.IRepositoryPrestamos;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,19 +28,25 @@ public class PrestamosController {
     @Autowired //inyección de dependencia: por tanto, será inicializado
     IRepositoryPrestamos prestamosRepository; //creando Spring automáticamente su instancia
 
+    @Autowired
+    IRepositoryHistorico historicoRepository;
+
     /**
      * Metodo de seleccion de todos los prestamos.
+     *
      * @return Devuelve todos los prestamos existentes.
      */
     @GetMapping //endpoint para buscar todos
     // Tipo de solicitud HTTP --> GET
     public List<EntityPrestamos> buscarPrestamos() {
         // ... (código para buscar todos los libros)
+        HistoricoHelper.guardarSentencia("IP", historicoRepository, "SELECT * FROM prestamos");
         return (List<EntityPrestamos>) prestamosRepository.findAll();
     }
 
     /**
      * Metodo para buscar un prestamo por su ID, donde {id} es un parámetro de ruta.
+     *
      * @param id parametro con el ID del prestamo
      * @return devuelve el prestamo o error dependiendo de si existe o no.
      */
@@ -46,13 +54,16 @@ public class PrestamosController {
     public ResponseEntity<EntityPrestamos> buscarLibroPorId(@PathVariable(value = "id") int id) {
         // ... (código para buscar un libro por su identificador)
         Optional<EntityPrestamos> prestamo = prestamosRepository.findById(id);
-        if (prestamo.isPresent())
-            return ResponseEntity.ok().body(prestamo.get()); // HTTP 200 OK
+
+        HistoricoHelper.guardarSentencia("IP", historicoRepository, "SELECT * FROM prestamos WHERE id=" + id);
+
+        if (prestamo.isPresent()) return ResponseEntity.ok().body(prestamo.get()); // HTTP 200 OK
         else return ResponseEntity.notFound().build(); // HTTP 404
     }
 
     /**
      * Metodo para insertar un prestamo.
+     *
      * @param prestamo parametro que contiene el objeto EntityPrestamo a insertar
      * @return inserta el prestamo.
      */
@@ -60,46 +71,49 @@ public class PrestamosController {
     public EntityPrestamos guardarPrestamo(@Valid @RequestBody EntityPrestamos prestamo) {
         prestamo.setIdPrestamo(0);
         prestamo.setFechaPrestamo(LocalDate.now());
+        HistoricoHelper.guardarSentencia("IP", historicoRepository, "INSERT INTO prestamos (idLibro, idUsuario, fechaPrestamo) VALUES (" + prestamo.getLibro().getId() + "," + prestamo.getUsuario().getId() + "," + prestamo.getFechaPrestamo() + ")");
         return prestamosRepository.save(prestamo); // Guarda el prestamo.
     }
 
     /**
      * Metodo para borrar un prestamo, donde {id} es un parámetro de ruta.
+     *
      * @param id parametro con el prestamo a eliminar
      * @return devuelve el prestamo o error dependiendo de si existe o no.
      */
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> borrarLibro (@PathVariable(value = "id") int id) {
+    public ResponseEntity<?> borrarLibro(@PathVariable(value = "id") int id) {
         // ... (código para borrar un prestamo por su id)
         Optional<EntityPrestamos> prestamo = prestamosRepository.findById(id);
-        if(prestamo.isPresent()) {
+        if (prestamo.isPresent()) {
+            HistoricoHelper.guardarSentencia("IP", historicoRepository, "DELETE FROM prestamos WHERE id=" + id);
             prestamosRepository.deleteById(id);
             return ResponseEntity.ok().body("Borrado");
-        }
-        else {
+        } else {
             return ResponseEntity.notFound().build();
         }
     }
 
     /**
      * Metodo para actualizar un prestamo existente, donde {id} es un parámetro de ruta.
+     *
      * @param nuevoPrestamo objeto EntityPrestamos que contiene los nuevos datos del prestamo
-     * @param id parametro con el prestamo a actualizar
+     * @param id            parametro con el prestamo a actualizar
      * @return devuelve el prestamo o error dependiendo de si existe o no.
      */
     // Tipo de solicitud HTTP --> PUT
     @PutMapping("/{id}")
-    public ResponseEntity<?> actualizarPrestamo(@Validated @RequestBody EntityPrestamos nuevoPrestamo,
-                                             @PathVariable(value = "id") int id) {
+    public ResponseEntity<?> actualizarPrestamo(@Validated @RequestBody EntityPrestamos nuevoPrestamo, @PathVariable(value = "id") int id) {
         Optional<EntityPrestamos> prestamo = prestamosRepository.findById(id);
         if (prestamo.isPresent()) {
+            HistoricoHelper.guardarSentencia("IP", historicoRepository, "UPDATE prestamos SET " + "idLibro=" + nuevoPrestamo.getLibro().getId() + "," + "idUsuario=" + nuevoPrestamo.getUsuario().getId() + "," + "fechaPrestamo" + nuevoPrestamo.getFechaPrestamo() + "WHERE id=" + id);
+
             prestamo.get().setFechaPrestamo(nuevoPrestamo.getFechaPrestamo());
             prestamo.get().setLibro(nuevoPrestamo.getLibro());
             prestamo.get().setUsuario(nuevoPrestamo.getUsuario());
             prestamosRepository.save(prestamo.get());
             return ResponseEntity.ok().body("Actualizado");
-        }
-        else {
+        } else {
             return ResponseEntity.notFound().build();
         }
     }
